c1912a581c4166008af3d115859501f53024e8c3
Mohammad Hafzah <mohammad@tycoonje.com>
2023-08-29 17:34:52 +0300

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Mohammad Hafzah on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
